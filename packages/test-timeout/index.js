142e3b95b892b8d83a7749c5a9b5c72c43b1a407
Sérgio Ramos <mail@sergioramos.me>
2017-02-10 00:30:29 +0000

    :umbrella: fix inconsistent timout order

--- packages/test-timeout/index.js
+++ packages/test-timeout/index.js
@@ -2,32 +2,35 @@ const buildArray = require('build-array');
 const timeout = require('timeout-then');
 const shuffle = require('array-shuffle');
 
-const getTimes = (size, interval) =>
-  shuffle(buildArray(size).map((v, i) => interval * (i + 1)));
-
-const _checkTimes = (times) => times.map((time, i) => {
-  const a = times[i - 1] || 0;
+const checkTimes = (times) => !times.map((time, i) => {
+  const a = times[i - 1] || -Infinity;
   const b = times[i + 1] || Infinity;
 
   const min = Math.min(a, b);
   const max = Math.max(a, b);
 
-  return !(time > min && time < max);
+  return time > min && time < max;
 }).every(Boolean);
 
-const checkTimes = (times) => times.length > 1
-  ? _checkTimes(times)
-  : false;
+const getTimes = (size, interval) => {
+  const times = shuffle(buildArray(size).map((v, i) => interval * (i + 1)));
 
-module.exports = (size) => {
+  if (times.length === 1) {
+    return times;
+  }
+
+  if (checkTimes(times)) {
+    return times;
+  }
+
+  return getTimes(size, interval);
+};
+
+module.exports = (size, label) => {
   const interval = size * 25;
-  let times;
-
-  do {
-    times = getTimes(size, interval);
-  } while (checkTimes(times));
+  const times = getTimes(size, interval);
 
   let i = 0;
 
-  return (v) => timeout(times[i++]).then(() => v);
+  return (v) => timeout(times[i++]).then(() => v)
 };
